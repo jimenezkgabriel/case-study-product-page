2861774091bb1e7701b06cccf2e0cc4a0ac05f0d
kaylaknight385 <kaylaknight385@gmail.com>
2025-12-01 20:46:12 -0500

    js dooone ayyyye

--- script.js
+++ script.js
@@ -19,4 +19,18 @@ if (signupForm) {
         e.preventDefault(); // so page wont reload
         
         const nameInput = document.getElementById('name');
-        const emailInput
+        const emailInput = document.getElementById('email');
+
+        const name = nameInput ? nameInput.value : '';
+        const email = emailInput ? emailInput.value : '';
+
+        if (name.trim() === '' || email.trim() === '') {
+            alert('Aye...fill out everything..! ALL FIELDS!');
+            return;
+        }
+
+        //  successfully signed up alert
+        alert(`Thanks for signing up, ${name}! We'll send book recommendations to ${email} soon!`);
+        });
+    };
+    
